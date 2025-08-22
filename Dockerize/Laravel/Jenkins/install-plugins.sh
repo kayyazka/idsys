1224a3886f4a253ddaf364c3ab37700b107a14b0
A.Sulaiman <rootblackwhite@gmail.com>
2025-08-22 14:55:31 +0700

    fix pipeline

--- Dockerize/Laravel/Jenkins/install-plugins.sh
+++ Dockerize/Laravel/Jenkins/install-plugins.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 set -e
 
-echo "==> Installing Jenkins plugins..."
+
+echo "== Installing Jenkins plugins =="
 jenkins-plugin-cli --plugin-file /usr/share/jenkins/ref/plugins.txt
 
-echo "==> Starting Jenkins..."
+echo "== Installing docker =="
+apt-get update  -y 
+apt-get install -y docker.io
+docker -v
+
+echo "== Starting Jenkins =="
 exec /usr/local/bin/jenkins.sh
+
+
+
+
